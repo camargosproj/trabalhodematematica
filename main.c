f78f75667e016bf88a0793cea827902f7ede2c99
Marcos Camargo <marcoscamargo@outlook.com>
2021-05-18 18:48:59 -0300

    Estrutura de repetiçao aninhada para calculo de quaquer ordem

--- main.c
+++ main.c
@@ -1,45 +1,69 @@
 #include <stdio.h>
 #include <stdbool.h>
-bool menu(){
-  int op;
-  printf("Digite o que você gostaria de fazer:\n");
-  printf("1 - Matriz de ordem 2\n");
-  printf("2 - Matriz de Ordem 3:\n");
-  printf("3 - Sair:\n");
-  scanf("%d",&op);
 
-  if(op == 1){
-    matrizesOrdem2();
-  }else if(op == 2){
-    matrizesOrdem2();
-  }else if(op == 3){
-    return false;
-  }
+int det(){
+int matriz[2][2] = 
+{{1,4},
+{3,2}};
 
 }
-int matrizesOrdem2(){
-  int matriz1[2][2];
-  int matriz2 [2][2];
+void matrizesOrdem(int ordem){
+  int matriz1[ordem][ordem];
+  int matriz2 [ordem][ordem];
+  int resultado[ordem][ordem];
+  int soma = 0;
   printf("Digite os valores da pimeira Matriz\n");
-  for (int i = 0; i < 2; i++){
-    for (int j = 0; j < 2; j++){
+  printf("%d\n", ordem);
+
+  for (int i = 0; i < ordem; i++){
+    for (int j = 0; j < ordem; j++){
       printf("Digite o elemento [%d] [%d]: ", i, j);
       scanf("%d", &matriz1[i][j]);
     }
   }
   printf("Digite os valores da segunda Matriz\n");
-  for (int i = 0; i < 2; ++i){
-    for (int j = 0; j < 2; ++j){
+  for (int i = 0; i < ordem; ++i){
+    for (int j = 0; j < ordem; ++j){
       printf("Digite o elemento [%d] [%d]: ", i, j);
       scanf("%d", &matriz2[i][j]);
     }
   }
-  int a11 = matriz1[0][0] * matriz2[0][0] + matriz1[0][1] * matriz2[1][0]; 
-  int a12 = matriz1[0][0] * matriz2[0][1] + matriz1[0][1] * matriz2[1][1];   
-  int a13 = matriz1[1][0] * matriz2[0][0] + matriz1[1][1] * matriz2[1][0]; 
-  int a14 = matriz1[1][0] * matriz2[0][1] + matriz1[1][1] * matriz2[1][1];  
-  printf("%d %d\n%d %d\n",a11,a12,a13,a14);
-     
+  for (int c = 0; c < ordem; c++) {
+      for (int d = 0; d < ordem; d++) {
+        for (int k = 0; k < ordem; k++) {
+          soma += matriz1[c][k]*matriz2[k][d];
+        } 
+        resultado[c][d] = soma;
+        soma = 0;
+      }
+  }
+  for (int c = 0; c < ordem; c++) {
+      for (int d = 0; d < ordem; d++)
+        printf("%d\t", resultado[c][d]);
+ 
+      printf("\n");
+    }
+
+}
+bool menu(){
+  int op;
+  printf("Digite o que você gostaria de fazer:\n");
+  printf("1 - Matriz de ordem 2\n");
+  printf("2 - Matriz de Ordem 3:\n");
+  printf("3 - Calcular Determinante:\n");
+  printf("4 - Sair:\n");
+  scanf("%d",&op);
+
+  if(op == 1){
+    matrizesOrdem(2);
+  }else if(op == 2){
+    matrizesOrdem(3);
+  }else if(op == 3){
+    det();
+  } else if(op == 4){
+    return false;
+  }
+
 }
 
 int main(void) {
@@ -51,5 +75,4 @@ int main(void) {
 
   }
   return 0;
-
 }
